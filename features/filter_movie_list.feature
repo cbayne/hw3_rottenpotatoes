2f9b9410777c3cb2eaebcf4b5c502e83262d3159
cbayne <christina.bayne@gmail.com>
2013-03-31 20:53:46 -0400

    Part 2

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -19,13 +19,14 @@ Background: movies have been added to database
   | Raiders of the Lost Ark | PG     | 12-Jun-1981  |
   | Chicken Run             | G      | 21-Jun-2000  |
 
-  And  I am on the RottenPotatoes home page
+   
+  And I am on the RottenPotatoes home page
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
-  When I check the following ratings: PG R
+  Given I check the following ratings: PG, R
   # enter step(s) to uncheck all other checkboxes
-  And I uncheck the following ratings: PG-13 G NC-17
+  When I uncheck the following ratings: PG-13, G, NC-17
   # enter step to "submit" the search form on the homepage
   And I submit the search form
   # enter step(s) to ensure that PG and R movies are visible
@@ -38,4 +39,7 @@ Scenario: no ratings selected
 
 Scenario: all ratings selected
   # see assignment
-  
+  When I check the following ratings: PG, R, PG-13, G, NC-17
+  And I submit the search form
+  Then I should see all of the movies
+
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,14 +4,64 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+  
   Movie.create!(movie)
-  end
-  assert false, "Unimplemented"
+  end 
   #flunk "Unimplemented"
 end
 
-Given /^I am on the RottenPotatoes home page$/ do 
-visit ('/movies')
+# Make sure that one string (regexp) occurs before or after another one
+#   on the same page
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+#  ensure that that e1 occurs before e2.
+#  page.content  is the entire content of the page as a string.
+        assert false, "Unimplmemented"
+end
+
+# Make it easier to express checking or unchecking several boxes at once
+#  "When I uncheck the following ratings: PG, G, R"
+#  "When I check the following ratings: G"
+
+Given /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  # HINT: use String#split to split up the rating_list, then
+  #   iterate over the ratings and reuse the "When I check..." or
+  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+ratings = rating_list.split(", ")
+ratings.each do |rating|
+   if uncheck then
+        uncheck("ratings_#{rating}")
+    else
+      check("ratings_#{rating}")
+    end
+  end
+end
+
+When /^I submit the search form$/ do
+  click_button "Refresh"
+end
+
+When /I sort the results by (.*)/ do |sort_order|
+  sort_id = sort_order.gsub(/\s/, '_')
+  click_link "#{sort_id}_header"
+end
+
+Then /^I should see PG and R movies$/ do
+  #page.should have_xpath("//td[text()='PG']")
+  assert page.has_xpath?("//td[text()='PG']")
+  assert page.has_xpath?("//td[text()='R']")
+end
+
+Then /^I should not see other movies$/ do
+  assert page.has_no_xpath?("//td[text()='PG-13']")
+  assert page.has_no_xpath?("//td[text()='G']")
+  assert page.has_no_xpath?("//td[text()='NC-17']")
+end
+
+Then /^I should see all of the movies$/ do
+  #puts page.body
+  assert page.has_css?("table tbody tr", count: 10)
+end
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
@@ -19,15 +69,9 @@ visit ('/movies')
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  flunk "Unimplemented"
+#assert page.body =~ /#{e1}.*{e2}/m
+  assert false, "Unimplemented"
 end
 
-# Make it easier to express checking or unchecking several boxes at once
-#  "When I uncheck the following ratings: PG, G, R"
-#  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-end
+
